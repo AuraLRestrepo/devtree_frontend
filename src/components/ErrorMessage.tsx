29ff7921e210a599a9ef2133817e037297106d8e
AuraLRestrepo <auralucia18@gmail.com>
2026-05-25 16:43:05 -0500

    login view and validation with back

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,11 +1,9 @@
-
-
-import React from "react";
-
 type Props = {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export const ErrorMessage: React.FC<Props> = ({ children }) => {
-    return <p className="text-red-500">{children}</p>;
+  return (
+    <p className="bg-red-50 text-red-600 p-3 uppercase text-sm font-bold text-center">{children}</p>
+  );
 };
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,27 +1,43 @@
-import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "../components/ErrorMessage";
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { isAxiosError } from 'axios';
+import { toast } from 'sonner';
+import type { RegisterForm } from '../types';
+import { ErrorMessage } from '../components/ErrorMessage';
+import api from '../config/axios';
 
-type RegisterFormValues = {
-  name: string;
-  email: string;
-  handle: string;
-  password: string;
-  password_confirmation: string;
+const RegisterFormValues: RegisterForm = {
+  name: '',
+  email: '',
+  handle: '',
+  password: '',
+  password_confirmation: '',
 };
 
 export const RegisterView = () => {
   const {
     register,
     watch,
+    reset,
     handleSubmit,
     formState: { errors },
-  } = useForm<RegisterFormValues>();
+  } = useForm({ defaultValues: RegisterFormValues });
 
-  console.log(errors);
-  const handleRegister = () => {
-    console.log("desde handleRegister");
+  const handleRegister = async (formData: RegisterForm) => {
+    try {
+      const response = await api.post('auth/register', formData);
+      toast.success(response.data);
+      reset();
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        toast.error(error.response?.data?.error);
+      } else {
+        console.log(error);
+      }
+    }
   };
+
+  const password = watch('password');
 
   return (
     <>
@@ -40,11 +56,9 @@ export const RegisterView = () => {
             type="text"
             placeholder="Tu Nombre"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register("name", { required: "El nombre es requerido" })}
+            {...register('name', { required: 'El nombre es requerido' })}
           />
-          {errors.name && (
-            <ErrorMessage>{errors.name.message}</ErrorMessage>
-          )}
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
         </div>
         <div className="grid grid-cols-1 space-y-3">
           <label htmlFor="email" className="text-2xl text-slate-500">
@@ -55,11 +69,15 @@ export const RegisterView = () => {
             type="email"
             placeholder="Email de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register("email", { required: "El email es requerido" })}
+            {...register('email', {
+              required: 'El email es requerido',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: 'E-mail no válido',
+              },
+            })}
           />
-          {errors.email && (
-            <ErrorMessage>{errors.email.message}</ErrorMessage>
-          )}
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </div>
         <div className="grid grid-cols-1 space-y-3">
           <label htmlFor="handle" className="text-2xl text-slate-500">
@@ -70,11 +88,9 @@ export const RegisterView = () => {
             type="text"
             placeholder="Nombre de usuario: sin espacios"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register("handle", { required: "El handle es requerido" })}
+            {...register('handle', { required: 'El handle es requerido' })}
           />
-          {errors.handle && (
-            <ErrorMessage>{errors.handle.message}</ErrorMessage>
-          )}
+          {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
         </div>
         <div className="grid grid-cols-1 space-y-3">
           <label htmlFor="password" className="text-2xl text-slate-500">
@@ -85,18 +101,16 @@ export const RegisterView = () => {
             type="password"
             placeholder="Password de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register("password", { required: "El password es requerido" })}
+            {...register('password', {
+              required: 'El password es requerido',
+              minLength: { value: 8, message: 'El password debe tener al menos 8 caracteres' },
+            })}
           />
-          {errors.password && (
-            <ErrorMessage>{errors.password.message}</ErrorMessage>
-          )}
+          {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
         </div>
 
         <div className="grid grid-cols-1 space-y-3">
-          <label
-            htmlFor="password_confirmation"
-            className="text-2xl text-slate-500"
-          >
+          <label htmlFor="password_confirmation" className="text-2xl text-slate-500">
             Repetir Password
           </label>
           <input
@@ -104,8 +118,9 @@ export const RegisterView = () => {
             type="password"
             placeholder="Repetir Password"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register("password_confirmation", {
-              required: "La confirmación del password es requerida",
+            {...register('password_confirmation', {
+              required: 'La confirmación del password es requerida',
+              validate: (value) => value === password || 'Los passwords no coinciden',
             })}
           />
           {errors.password_confirmation && (
